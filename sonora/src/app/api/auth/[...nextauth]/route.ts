f42bdfa71c083461f1db3772f1dd2b397ea5cf69
Rayis B <rayis.b@icloud.com>
2025-09-10 20:54:47 +0200

    feat: setup NextAuth with Spotify provider

---
+++ sonora/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+
+const handler = NextAuth({
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      authorization:
+        "https://accounts.spotify.com/authorize?scope=user-read-email playlist-read-private",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
