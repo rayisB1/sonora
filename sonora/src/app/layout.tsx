0fb0ba308a00df8f4215d587648bb053ea25ff94
Rayis B <rayis.b@icloud.com>
2025-09-10 20:55:43 +0200

    feat: wrap application with SessionProvider for authentication

--- sonora/src/app/layout.tsx
+++ sonora/src/app/layout.tsx
@@ -1,24 +1,17 @@
+"use client";
+
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
 import { Righteous } from "next/font/google";
 
-const righteous = Righteous({
-  subsets: ["latin"],
-  weight: "400",
-});
+const righteous = Righteous({ subsets: ["latin"], weight: "400" });
 
-export const metadata = {
-  title: "Sonora",
-  description: "Mood-based music app",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={righteous.className}>{children}</body>
+      <body className={righteous.className}>
+        <SessionProvider>{children}</SessionProvider>
+      </body>
     </html>
   );
 }
