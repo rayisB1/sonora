da9f593839a83e6dbfa499374ae599e65ef0779f
Rayis B <rayis.b@icloud.com>
2025-09-10 20:58:08 +0200

    feat: integrate Navbar into layout with Spotify login/logout

--- sonora/src/app/layout.tsx
+++ sonora/src/app/layout.tsx
@@ -3,6 +3,7 @@
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Righteous } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 const righteous = Righteous({ subsets: ["latin"], weight: "400" });
 
@@ -10,7 +11,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={righteous.className}>
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          <Navbar />
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
